d559d634058b0feede9a822b30aa56557da049dc
Elham <elhamwaheed777@gmail.com>
2025-03-13 01:56:08 +0500

    Hero Testing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,11 +9,14 @@ const Hero = () => {
       <div className="absolute"></div>
 
       {/* Content */}
-      <div className="relative top-[30px] md:top-[135px] lg:top-[220px] z-10 px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <div className="relative top-[-10px] md:top-[110px] lg:top-[190px] z-10 px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
         <div className="flex items-center gap-2 pb-3 md:pb-5">
           <h1 className="font-bold text-2xl md:text-4xl lg:text-6xl">RELIABILITY IN EVERY DETAIL</h1>
         </div>
-        <p className="text-sm md:text-base lg:text-lg sm:w-2/4">Depend on our meticulous operation and maintenance services. Enhancing reliability and minimizing downtime for your operations.</p>
+        <p className="text-xs md:text-base lg:text-lg sm:w-4/5">Depend on our meticulous operation and maintenance services. Enhancing reliability and minimizing downtime for your operations.</p>
+        <Link to="/contact">
+          <button className="bg-white text-black hover:bg-black hover:text-white w-auto max-w-fit tracking-wide my-4 transition-all duration-300 ease-in-out text-[10px] px-3 py-2  lg:text-[14px] lg:px-6 lg:py-3">CONTACT US</button>
+        </Link>
       </div>
     </div>
   );
