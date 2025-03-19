1cf33ddf00a90fefe11431200f2873ba4713d6c1
MuhammadElham <elhamwaheed777@gmail.com>
2025-03-19 10:39:40 -0700

    Add Banner Image in Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ContactForm from "../components/ContactForm";
+import Hero_About from "../components/Hero_About";
 
 const Contact = () => {
   return (
     <div>
+      <Hero_About bgImg={'https://www.magnet.co.za/news/cms-assets/inline-images/Pg-68-1.jpg'} heading={'Get in Touch with Us'}/>
       <ContactForm />
     </div>
   );
