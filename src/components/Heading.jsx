0c86400cc74de79ecca1c48234a73ba48e85f9c3
MuhammadElham <elhamwaheed777@gmail.com>
2025-03-22 04:16:07 -0700

    Woking in Company Page

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,14 +1,10 @@
 import { motion } from "framer-motion";
 
-const Heading = ({ text }) => {
+const Heading = ({ children, text, className }) => {
   return (
-    <motion.h2
-      className="text-center text-2xl sm:text-3xl font-bold mb-12"
-      initial={{ opacity: 0, y: -20 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.6, ease: "easeOut" }}
-    >
-      {text}
+    <motion.h2 className={`${className} text-center text-2xl sm:text-3xl font-bold mb-12 `} initial={{ opacity: 0, y: -20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6, ease: "easeOut" }}>
+      {children && <>{children}</>}
+      {text && <>{text}</>}
     </motion.h2>
   );
 };
--- src/pages/CompanyProducts.jsx
+++ src/pages/CompanyProducts.jsx
@@ -11,16 +11,38 @@ const CompanyProducts = () => {
 
   return (
     <div className="py-14">
-      <Heading text={`Products by ${company.name}`} />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {company.products.map((product) => (
-          <div key={product.id} className="shadow-lg p-6 bg-white rounded-lg cursor-pointer hover:scale-105 transition-transform" onClick={() => navigate(`/product/${company.id}/${product.id}`)}>
-            <img src={product.image} alt={product.name} className="w-full h-32 object-contain" />
-            <h3 className="text-xl font-semibold mt-4">{product.name}</h3>
-            <p className="text-gray-600">{product.power}</p>
-          </div>
-        ))}
+      <Heading className={"lg:mb-9 lg:text-[55px] !text-left"}>
+        Products by <span className="text-primary">{company.name}</span>
+      </Heading>
+      <p className="text-lg font-medium">
+        The company "MaxMotors" offers environmentally friendly and safe Jenbacher gas piston units. We are the official distributor of INNIO Jenbacher in Russia. We offer design and installation from
+        scratch at any type of production: in medical organizations, office and shopping centers, in agricultural production, for private boiler houses. You can see the models of gas piston units in
+        our catalog. For detailed advice and preliminary calculation, call 8 800 505-77-99 .
+      </p>
+   
+   <div className="grid grid-cols-1 md:grid-cols-2 gap-8 py-14">
+  {company.products.map((product) => (
+    <div
+      key={product.id}
+      className="relative h-[400px] bg-[#222] rounded-lg p-6 cursor-pointer hover:scale-105 transition-transform"
+      onClick={() => navigate(`/product/${company.id}/${product.id}`)}
+    >
+      <div className="absolute left-0 top-1/2 transform -translate-y-1/2 w-[80%]">
+        <img
+          src={product.image}
+          alt={product.name}
+          className="w-full h-auto object-cover"
+          />
       </div>
+
+      <div className="relative flex flex-col items-end text-white pr-10">
+        <h3 className="text-4xl font-bold">{product.power}</h3>
+        <h3 className="text-3xl font-bold">{product.name}</h3>
+      </div>
+    </div>
+  ))}
+</div>
+
     </div>
   );
 };
